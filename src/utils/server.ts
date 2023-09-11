6173a09c7238eb9e65fa5b05e440a1ebfbcf27b5
t <pptsuwit.gmail.com>
2023-09-11 15:26:45 +0700

    feat: cors

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,13 @@
 const express = require("express");
+import cors from "cors";
 import errorHandler from "../middlewares/error-handler";
 import userControllerRouter from "../controllers/user.controller";
 import authControllerRouter from "../controllers/auth.controller";
 import customerControllerRouter from "../controllers/customer.controller";
 function createServer() {
   const app = express();
+  app.use(cors());
+  app.options("*", cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use("/api", userControllerRouter);
